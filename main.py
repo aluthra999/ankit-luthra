a7180f8c407c9f70753af4b8a0be5b6c5624e406
Ankit Luthra <95644464+aluthra999@users.noreply.github.com>
2023-10-25 01:35:35 -0300

    Update 10/25/23
    Updates:
    Added database to hold resume info
    
    resume.html:
    made it more dynamic than hard coded html, now it; get info from database
    
    added pdf resume link

--- main.py
+++ main.py
@@ -1,12 +1,47 @@
 from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap5
 from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 app.config['SECRET_KEY'] = "thiskeyisverysecret"
+# SQLite database file
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///MyInfo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # To suppress warnings
+
+db = SQLAlchemy(app)
 
 current_year = datetime.now().year
+
+# Define the Info table
+
+
+class Info(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    skills = db.Column(db.String(255), unique=True, nullable=False)
+    programming_languages = db.Column(
+        db.String(255), unique=True, nullable=False)
+    languages = db.Column(db.String(255), unique=True, nullable=False)
+    interests = db.Column(db.String(255), unique=True, nullable=False)
+
+
+# Define the Education table
+class Education(db.Model):
+    id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
+    from_date = db.Column(db.String(50), nullable=False)
+    to_date = db.Column(db.String(50), nullable=False)
+    college_name = db.Column(db.String(255), nullable=False)
+    city = db.Column(db.String(100), nullable=False)
+    province = db.Column(db.String(100), nullable=False)
+    degree = db.Column(db.String(100), nullable=False)
+    major = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
+
+
+# Create the database and the table
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
@@ -16,7 +51,10 @@ def home():
 
 @app.route("/resume")
 def resume():
-    return render_template('resume.html', year=current_year)
+    # Retrieve data from the 'Info' table
+    info_data = Info.query.all()
+    education_data = Education.query.all()
+    return render_template('resume.html', year=current_year, info_data=info_data, education_data=education_data)
 
 
 @app.route("/projects")
